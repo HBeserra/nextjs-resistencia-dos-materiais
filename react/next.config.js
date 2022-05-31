ac13bc90136633b38428a5ee2df38eca944381bf
Herbert <Herbert.beserra@gmail.com>
2022-05-31 19:26:33 -0300

    Feat: pwa

---
+++ react/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
